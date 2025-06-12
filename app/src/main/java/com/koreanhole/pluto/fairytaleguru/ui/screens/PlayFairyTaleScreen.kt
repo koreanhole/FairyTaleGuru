9d938cbf32de5f24ffd1015475a005f0597befd8
Soonhyeong Kwon <koreanhole1@gmail.com>
2025-06-12 17:45:15 +0900

    Refactor: Remove unused imports from PlayFairyTaleScreen
    This commit removes unused imports from `PlayFairyTaleScreen.kt`, specifically:
    - `androidx.compose.material.icons.Icons`
    - `androidx.compose.material.icons.automirrored.filled.ArrowBack`
    - `androidx.compose.material3.Icon`
    - `androidx.compose.material3.IconButton`

--- app/src/main/java/com/koreanhole/pluto/fairytaleguru/ui/screens/PlayFairyTaleScreen.kt
+++ app/src/main/java/com/koreanhole/pluto/fairytaleguru/ui/screens/PlayFairyTaleScreen.kt
@@ -4,11 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
